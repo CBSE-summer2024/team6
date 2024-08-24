b0375f9e616fb7cc4ec9077dda99be38b9806e88
Ibtihaj7 <ibtihaj.adham7@gmail.com>
2024-08-24 21:20:31 +0300

    Setup The Project

---
+++ declarative_mobile_frameworks-main/android/app/src/main/kotlin/com/example/native_navigation/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.native_navigation
+
+import android.content.Intent
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity : FlutterFragmentActivity()
