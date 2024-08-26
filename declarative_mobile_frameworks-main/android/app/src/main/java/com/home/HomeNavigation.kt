f07f82c102dbf917d7d3fe9a70416d2cc747cf58
Tasnim <s12028850@stu.najah.edu>
2024-08-26 20:45:08 +0300

    Create App Navigation

---
+++ declarative_mobile_frameworks-main/android/app/src/main/java/com/home/HomeNavigation.kt
@@ -0,0 +1,19 @@
+package com.example.native_navigation.home
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+
+const val HOME_ROUTE = "home_route"
+
+fun NavController.navigateToHome(navOptions: NavOptions? = null) =
+    navigate(HOME_ROUTE, navOptions)
+
+fun NavGraphBuilder.homeScreen(
+    onSearchClicked: () -> Unit
+) {
+    composable(route = HOME_ROUTE) {
+        HomeScreen(onSearchClicked = onSearchClicked)
+    }
+}
